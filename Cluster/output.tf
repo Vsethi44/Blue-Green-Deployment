cce55ecf2b89f9fe8f029a19dc41224e4715b51f
Vishawnath Sethi <151629020+Vsethi44@users.noreply.github.com>
2024-10-11 01:28:18 +0530

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.mykubeproject.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.mykubeproject.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.mykubeproject_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.mykubeproject_subnet[*].id
 }
-
